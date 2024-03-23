767625686f248f6ede80a62b450ca338f64f7838
MateusAlbuquerqueps <maateusalbuquerque99@gmail.com>
2024-03-23 20:57:02 -0300

    Padronizando as páginas HTML

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -31,8 +31,7 @@ public class FilmeController {
         var filme = new Filme(dados);
         filmes.add(filme);
 
-        System.out.println(filmes);
-        return "filmes/formulario";
+        return "redirect:/filmes";
     }
 
 }
