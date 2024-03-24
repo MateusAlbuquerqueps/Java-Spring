dfcb4b36d86aab3c2c864d65372da17ad542434a
MateusAlbuquerqueps <maateusalbuquerque99@gmail.com>
2024-03-24 10:21:49 -0300

    Adicionando o metodo Delete no projeto

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -6,6 +6,7 @@ import br.com.alura.screenmatch.domain.filme.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,13 @@ public class FilmeController {
         var filme = new Filme(dados);
 
         repository.save(filme);
+
+        return "redirect:/filmes";
+    }
+
+    @DeleteMapping
+    public String removeFilme(Long id) {
+        repository.deleteById(id);
 
         return "redirect:/filmes";
     }
